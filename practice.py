a605bdc70a5301262b970aad3c4d6267e8a51def
AmirAX17 <amirjahir0279@gmail.com>
2024-09-26 17:21:23 +1000

    app
    app

--- practice.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("enter first number"))
-num2 = int(input("enter second number"))
-age = num1 + num2
-print(age)
-if(age<21):
-    print("You are underage")
-else:
-    print("Access granted")
